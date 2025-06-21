48049bc77eb415e58f4449fade45d6a7cec3cda1
Treidex <treidexy@gmail.com>
2025-06-21 11:12:22 -0700

    it's sad that the toddlers who made godot had to flunk kindergarten

---
+++ scripts/troops/capu/capu_troop.gd
@@ -0,0 +1,30 @@
+class_name CapuTroop
+extends Troop
+
+@export var SLASH: PackedScene = preload("res://scenes/troops/tung/tung_slash.tscn");
+@export var THROW: PackedScene = preload("res://scenes/troops/tung/tung_throw.tscn");
+
+func main(dir: Vector2):
+	super(dir);
+	var proj: TungSlash = SLASH.instantiate();
+	proj.FATHER = self;
+	proj.life = 0.3;
+	proj.position = position;
+	proj.rotation = dir.angle();
+	if stat is TungStat:
+		proj.damage = stat.MAIN_DAMAGE;
+		proj.kb = stat.KB;
+	get_tree().current_scene.add_child(proj);
+
+func zuper(dir: Vector2):
+	super(dir);	
+	var proj: TungThrow = THROW.instantiate();
+	proj.FATHER = self;
+	proj.life = 3.0;
+	proj.position = position;
+	proj.rotation = dir.angle();
+	if stat is TungStat:
+		proj.velocity = dir * stat.THROW_SPEED;
+		proj.damage = stat.SUPER_DAMAGE;
+		proj.kb = stat.KB;
+	get_tree().current_scene.add_child(proj);
