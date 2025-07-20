b99989bf69bfa82c883a063cb12ad5f8ee5d1bbc
Treidex <treidexy@gmail.com>
2025-07-19 17:17:31 -0700

    fruli hyper snares

--- scripts/troops/fruli/fruli_stat.gd
+++ scripts/troops/fruli/fruli_stat.gd
@@ -2,6 +2,7 @@ class_name FruliStat
 extends Stat
 
 @export var WIND_DAMAGE: float = 900;
+@export var WIND_SNARE_DURATION: float = 0;
 @export var SWOOP_SPEED: float = 300;
 @export var SWOOP_DIST: float = 110; # hard coded in fruli_troop scene
 @export var SNATCH_DAMAGE: float = 1000;
--- scripts/troops/fruli/fruli_wind_projectile.gd
+++ scripts/troops/fruli/fruli_wind_projectile.gd
@@ -1,2 +1,15 @@
 class_name FruliWindProjetile
 extends Projectile
+
+func hit(victim: Troop):
+	super(victim);
+	
+	if victim == FATHER:
+		return;
+	
+	var stat: FruliStat = FATHER.stat;
+	if stat.WIND_SNARE_DURATION > 0:
+		var action = TimedAction.new();
+		action.does_silence = false;
+		action.life = stat.WIND_SNARE_DURATION;
+		get_tree().root.add_child(action);
