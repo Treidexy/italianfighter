e1658d8586073abfdba77401e3e6693be44e5c5e
Treidex <treidexy@gmail.com>
2025-08-18 10:53:03 -0700

    swoop no hit wall

--- scripts/actions/swoop_action.gd
+++ scripts/actions/swoop_action.gd
@@ -12,10 +12,15 @@ var _origen: Vector2;
 func begin():
 	super();
 	_origen = VICTIM.position;
+	VICTIM.set_collision_mask_value(3, false); # walls
 
 func _physics_process(delta: float) -> void:
 	super(delta);
 	VICTIM.velocity = velocity;
-	VICTIM.move_and_collide(VICTIM.velocity);
+	VICTIM.move_and_slide();
 	if _origen.distance_to(VICTIM.position) > dist:
 		end();
+
+func end():
+	super();
+	VICTIM.set_collision_mask_value(3, true);
